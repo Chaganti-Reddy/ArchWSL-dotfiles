192a461c785aa35120662ec5e13898bdecfd46e9
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-03-10 16:32:14 +0530

    feat: add Harpoon plugin with keybindings

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -131,4 +131,19 @@ return {
       end, { desc = '[E]xplorer (cwd)' })
     end,
   },
+
+{
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {},
+  keys = {
+    { '<leader>ha', function() require('harpoon'):list():add() end, desc = '[H]arpoon [A]dd' },
+    { '<leader>hh', function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end, desc = '[H]arpoon [M]enu' },
+    { '<leader>h1', function() require('harpoon'):list():select(1) end, desc = '[H]arpoon [1]' },
+    { '<leader>h2', function() require('harpoon'):list():select(2) end, desc = '[H]arpoon [2]' },
+    { '<leader>h3', function() require('harpoon'):list():select(3) end, desc = '[H]arpoon [3]' },
+    { '<leader>h4', function() require('harpoon'):list():select(4) end, desc = '[H]arpoon [4]' },
+  },
+},
 }
